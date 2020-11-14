e8ec91f1b128a77e66d5765c5c646993147e3bed
SatrioKuntoBirowo_1181400001 <65351175+SatrioKuntoBirowo118140001@users.noreply.github.com>
2020-11-14 18:31:55 +0700

    Add files via upload

---
+++ Modul 4/tugas/php/latihanphp1.php
@@ -0,0 +1,30 @@
+<?php
+    $a=20;
+    $b=5;
+
+    echo "Berikut merupakan hasil dari setiap operasi <br>";
+    $c="+";
+    echo "<br>PENJUMLAHAN<br>";
+    echo "operator : $c <br>";
+    echo "Hasil : " . ($a + $b) . "<br>";
+
+    $c="-";
+    echo "<br>PENGURANGAN<br>";
+    echo "operator : $c <br>";
+    echo "Hasil : " . ($a - $b). "<br>";
+
+    $c="*";
+    echo "<br>PERKALIAN<br>";
+    echo "operator : $c <br>";
+    echo "Hasil : " . ($a * $b). "<br>";
+
+    $c="/";
+    echo "<br>PENJUMLAHAN<br>";
+    echo "operator : $c <br>";
+    echo "Hasil : " . ($a / $b). "<br>";
+
+    $c="%";
+    echo "<br>MODULUS<br>";
+    echo "operator : $c <br>";
+    echo "Hasil : " . ($a % $b). "<br>";
+?>
